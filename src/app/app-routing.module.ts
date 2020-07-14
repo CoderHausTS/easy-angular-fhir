08c381f0cc03acd4fee64be17a5c9c97281105ae
Jason Woods <jason.woods@datatri.be>
2020-07-14 07:38:31 -0500

    Added patient lookup and some routing stuff

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ConformanceComponent } from './conformance/conformance.component';
 import { AuthComponent } from './auth/auth.component';
+import { PatientComponent } from './patient/patient.component';
 
 const routes: Routes = [
   {
@@ -12,6 +13,15 @@ const routes: Routes = [
     path: '',
     component: AuthComponent,
     pathMatch: 'full',
+  },
+  {
+    path: 'patient/:oid/:token',
+    component: PatientComponent,
+  },
+  {
+    path: 'patient',
+    component: PatientComponent,
+    pathMatch: 'full'
   }
 ];
 
